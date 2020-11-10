f8c165135413a825f8460e23fc8e37b95fc273df
Daniel Bielech <38240407+daCFniel@users.noreply.github.com>
2020-11-10 23:40:09 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * Main class to run the application.
+ *
+ * @author Daniel Bielech
+ * @version 02.04.2020
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ChasingBombs game = new ChasingBombs();
+    }
+}
